7d16785e554fb08749df8fcba37d91a367638a38
unknown <brooksjustind@gmail.com>
2019-03-25 20:48:21 -0400

    introducing app

---
+++ angular-mix/src/app/mock-recipes.ts
@@ -0,0 +1,7 @@
+import { Recipe } from './recipe';
+
+export const RECIPES: Recipe[] = [
+  { id: 11, name: 'Soup', ingredients:["stock","veggie","spice"] },
+  { id: 12, name: 'Carnitas', ingredients:["juice","spices","pork butt"] },
+  { id: 13, name: 'Chicken', ingredients:["marinade","chicken","spices"] }
+];
---
+++ angular-mix/src/app/recipe.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+import { RECIPES } from './mock-recipes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  constructor() { }
+
+  getRecipes(): Observable<Recipe[]> {
+    return of(RECIPES);
+  }
+
+  getRecipe(id: number): Observable<Recipe> {
+    return of(RECIPES.find(recipe => recipe.id === id));
+  }
+}
